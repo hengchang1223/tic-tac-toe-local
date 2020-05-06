e68ec45f35746a1049d92f8343242b218a221fd5
hengchang1223 <hengchang1223@gmail.com>
2020-05-06 02:42:48 -0400

    change from px to vw

--- src/index.js
+++ src/index.js
@@ -612,16 +612,16 @@ class Game extends React.Component {
     const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
-    const moves = history.map((step, move) => {
-      const desc = move ?
-        'Go to move #' + move :
-        'Go to game start';
-        return (
-          <li key={move}>
-            <button onClick={() => this.jumpTo(move)}>{desc}</button>
-          </li>
-        );
-    });
+    // const moves = history.map((step, move) => {
+    //   const desc = move ?
+    //     'Go to move #' + move :
+    //     'Go to game start';
+    //     return (
+    //       <li key={move}>
+    //         <button onClick={() => this.jumpTo(move)}>{desc}</button>
+    //       </li>
+    //     );
+    // });
 
     let status;
     if (winner) {
